4922ffae9df8806705fc6438c46595bc2fbcc2f7
sinedviper <sinedviper@gmail.com>
2023-01-03 21:30:59 +0200

    add header in hardcore page

--- src/app/assets/svg/index.ts
+++ src/app/assets/svg/index.ts
@@ -1,7 +1,9 @@
 import { ReactComponent as Loading } from './loading.svg'
 import { ReactComponent as Logo } from './logo.svg'
 import { ReactComponent as Discord } from './discord.svg'
+import { ReactComponent as Heart } from './heart.svg'
 
 export const LoadingIcon = Loading
 export const LogoIcon = Logo
+export const HeartIcon = Heart
 export const DiscordIcon = Discord
--- src/pages/hardcore/index.tsx
+++ src/pages/hardcore/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react'
 
 import { CardPage } from 'shared/cardPage'
+import { Button } from 'shared'
 
-//import styles from './styles.module.scss'
+import styles from './styles.module.scss'
 
 const HardcorePage = (): JSX.Element => {
-  return <CardPage>HardcorePage</CardPage>
+  return (
+    <CardPage className={styles.wrapperHardcore}>
+      <h2>Хардкор</h2>
+      <section className={styles.wrapperLife}>
+        <section className={styles.wrapperInfo}>
+          <div className={styles.infoFirst}>
+            <Button>Скопировать IP-адрес</Button>
+            <div className={styles.minecraftInfo}>
+              <p>License</p>
+              <p>1.19.2</p>
+              <p>Minecraft:JE</p>
+            </div>
+          </div>
+          <div className={styles.infoSecond}>
+            <h2>Minecraft</h2>
+            <h2>Vanilla Hardcore SMP</h2>
+          </div>
+        </section>
+      </section>
+    </CardPage>
+  )
 }
 
 export default HardcorePage
