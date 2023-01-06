5117dcb66ab33524c575d63b5e2e89d82b208cbf
sinedviper <sinedviper@gmail.com>
2023-01-06 23:05:43 +0200

    fix small bugs, add hardcore info in profile and add pages in profile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { ContentCopy } from '@mui/icons-material'
 import { useNavigate } from 'react-router-dom'
+import cn from 'classnames'
 
 import { CardPage } from 'shared/cardPage'
 import { useAppDispatch, useAppSelector, useExitAccount, useNotification } from 'processes/hooks'
@@ -9,6 +10,7 @@ import { getUser, getUserRoles } from 'processes/store/select'
 import { Button, CardInfo, Input } from 'shared'
 
 import styles from './Profile.module.scss'
+import { HeartIcon } from '../../app/assets/svg'
 
 const ProfilePage = (): JSX.Element => {
   const dispatch = useAppDispatch()
@@ -18,6 +20,7 @@ const ProfilePage = (): JSX.Element => {
 
   const [password, setPassword] = useState('')
   const [passwordError, setPasswordError] = useState(false)
+  const [navMenu, setNavMenu] = useState('setting')
 
   const user = useAppSelector(getUser)
   const userRoles = useAppSelector(getUserRoles)
@@ -62,51 +65,108 @@ const ProfilePage = (): JSX.Element => {
     <CardPage>
       <CardInfo className={styles.wrapperProfile}>
         <h2>Профиль {userRoles && userRoles.includes('admin') && ' (Админ)'}</h2>
-        {user && (
-          <div className={styles.wrapperInfoProfile}>
-            <span>
-              Никнейм:{' '}
-              <button className={styles.wrapperButton} onClick={() => handleCopy(user.username)}>
-                <ContentCopy />
-                <span>{user.username}</span>
-              </button>
-            </span>
-            <span>
-              Почта:{' '}
-              <button className={styles.wrapperButton} onClick={() => handleCopy(user.mail)}>
-                <ContentCopy />
-                <span>{user.mail}</span>
-              </button>
-            </span>
-            <div className={styles.changePassword}>
-              <p>Смена пароля</p>
-              <Input
-                value={password}
-                setValue={setPassword}
-                error={passwordError}
-                label={'Новый пароль'}
-                password={true}
-              />
-              <Button
-                className={styles.wrapperChangePass}
-                onClick={() => handleChangePassword(password, setPasswordError)}
-              >
-                Подтвердить
-              </Button>
-            </div>
-            {userRoles && userRoles.includes('admin') && (
-              <div className={styles.adminWrapper}>
-                <p>Админ панель</p>
-                <Button className={styles.wrapperButtonAdmin} onClick={() => navigate('/admin')}>
-                  Перейти
-                </Button>
-              </div>
-            )}
-            <Button className={styles.wrapperOut} onClick={() => handleExit()}>
-              Выйти из аккаунта
-            </Button>
+        <div className={styles.wrapperNav}>
+          <div className={styles.navLink}>
+            <button
+              className={cn({
+                [styles.buttonOn]: navMenu === 'setting',
+              })}
+              onClick={() => setNavMenu('setting')}
+            >
+              <span className={styles.firstT}></span>
+              <span className={styles.secondT}></span>
+              Настройки
+            </button>
+            <button
+              className={cn({
+                [styles.buttonOn]: navMenu === 'hardcore',
+              })}
+              onClick={() => setNavMenu('hardcore')}
+            >
+              <span className={styles.firstT}></span>
+              <span className={styles.secondT}></span>
+              Хардкор
+            </button>
           </div>
-        )}
+          {user && (
+            <div className={styles.wrapperInfoProfile}>
+              {navMenu === 'setting' && (
+                <div className={styles.wrapperSetting}>
+                  <span>
+                    Никнейм:{' '}
+                    <button
+                      className={styles.wrapperButton}
+                      onClick={() => handleCopy(user.username)}
+                    >
+                      <ContentCopy />
+                      <span>{user.username}</span>
+                    </button>
+                  </span>
+                  <span>
+                    Почта:{' '}
+                    <button className={styles.wrapperButton} onClick={() => handleCopy(user.mail)}>
+                      <ContentCopy />
+                      <span>{user.mail}</span>
+                    </button>
+                  </span>
+                  <div className={styles.changePassword}>
+                    <p>Смена пароля</p>
+                    <Input
+                      value={password}
+                      setValue={setPassword}
+                      error={passwordError}
+                      label={'Новый пароль'}
+                      password={true}
+                    />
+                    <Button
+                      className={styles.wrapperChangePass}
+                      onClick={() => handleChangePassword(password, setPasswordError)}
+                    >
+                      Подтвердить
+                    </Button>
+                  </div>
+                  {userRoles && userRoles.includes('admin') && (
+                    <div className={styles.adminWrapper}>
+                      <p>Админ панель</p>
+                      <Button
+                        className={styles.wrapperButtonAdmin}
+                        onClick={() => navigate('/admin')}
+                      >
+                        Перейти
+                      </Button>
+                    </div>
+                  )}
+                  <Button className={styles.wrapperOut} onClick={() => handleExit()}>
+                    Выйти из аккаунта
+                  </Button>
+                </div>
+              )}
+              {navMenu == 'hardcore' && (
+                <div className={styles.wrapperHardCore}>
+                  <iframe src='https://giphy.com/embed/0hChp6z6lE0IRBYTJG' />
+                  <h3>Статистика</h3>
+                  <div>
+                    <div>
+                      <div>
+                        <p>Примерное время возрождение после смерти: </p>
+                        <p>00:06:52:30</p>
+                      </div>
+                      <div>
+                        <p>Дата последней смерти: не умирал</p>
+                        <p>Причина последней смерти: не умирал</p>
+                      </div>
+                    </div>
+                    <div>
+                      <p>Смертей</p>
+                      <HeartIcon />
+                      <p>0</p>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
       </CardInfo>
     </CardPage>
   )
