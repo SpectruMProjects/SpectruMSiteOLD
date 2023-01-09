832b96a0851feb7be1c9a3b2fdc9054fca987221
sinedviper <sinedviper@gmail.com>
2023-01-09 20:51:39 +0200

    small add

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react'
 
 import { CardPage } from 'shared/cardPage'
 import { useNavigate } from 'react-router-dom'
+import { useAppSelector } from 'processes/hooks'
+import { getUserRoles } from 'processes/store/select'
+import { CardInfo } from 'shared'
 
 //import styles from './styles.module.scss'
 
@@ -9,14 +12,19 @@ const AdminPage = (): JSX.Element => {
   const navigate = useNavigate()
 
   const token = localStorage.getItem('accessToken')
+  const userRoles = useAppSelector(getUserRoles)
 
   useEffect(() => {
-    if (!token) {
+    if (!token && !userRoles.includes('admin')) {
       navigate('/')
     }
   }, [])
 
-  return <CardPage>AdminPage</CardPage>
+  return (
+    <CardPage>
+      <CardInfo>admin</CardInfo>
+    </CardPage>
+  )
 }
 
 export default AdminPage
